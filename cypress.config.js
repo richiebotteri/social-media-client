ad5826bb69cbaf9a84383c355dddc5fb1f9c0fe6
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-01-27 13:28:14 +0100

    adds env variables to cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,18 @@
-import eslint from "eslint";
-import process from "process";
 import { defineConfig } from "cypress";
 import dotenv from "dotenv";
+import process from "process";
+dotenv.config();
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      dotenv.config();
-      config.env = { ...config.env, ...process.env };
-      on("file:preprocessor", eslint());
+    setupNodeEvents() {
+      // implement node event listeners here
+    },
+    env: {
+      API_USERNAME: process.env.API_USERNAME,
+      API_EMAIL: process.env.API_EMAIL,
+      API_PASSWORD: process.env.API_PASSWORD,
+      API_TOKEN: process.env.API_TOKEN,
     },
   },
 });

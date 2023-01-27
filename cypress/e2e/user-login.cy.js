5f8b243b48832caa10aec981ce865731d2f504f0
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-01-27 16:24:16 +0100

    adds cypress user-login test

---
+++ cypress/e2e/user-login.cy.js
@@ -0,0 +1,31 @@
+describe("login feature", () => {
+  before(() => {
+    // Opens up website and closes register form
+    cy.visit("/");
+    cy.wait(2000);
+    cy.get('.modal-footer > [data-bs-dismiss="modal"]').eq(1).click();
+
+    // Opens up login form
+    cy.get('.text-end > [data-bs-target="#loginModal"]').click();
+    cy.wait(2000);
+  });
+
+  it("can login if the user submits valid credentials", () => {
+    // Adds and checks login input credentials
+    cy.get("#loginEmail")
+      .type(Cypress.env("API_EMAIL"))
+      .should("have.value", Cypress.env("API_EMAIL"), { force: true });
+    cy.get("#loginPassword")
+      .type(Cypress.env("API_PASSWORD"))
+      .should("have.value", Cypress.env("API_PASSWORD"));
+    cy.get("#loginForm > .modal-footer").find("button.btn-success").click();
+
+    // Checks that the login credentials are valid
+    cy.intercept("https://nf-api.onrender.com/api/v1/social/auth/login").as(
+      "userAuth"
+    );
+    cy.wait("@userAuth").then((userAuthResult) => {
+      expect(userAuthResult.response.statusCode).to.equal(200);
+    });
+  });
+});

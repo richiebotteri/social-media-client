b8f5396fef2c38f7fde61d4494b63f0a1682736d
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-01-29 16:35:34 +0100

    removes mock for storage

--- src/js/api/auth/__tests__/login.test.js
+++ src/js/api/auth/__tests__/login.test.js
@@ -1,36 +1,7 @@
 import { save } from "../../../storage";
 import { login } from "../login";
 
-jest.mock("../../../storage/index.js", () => {
-  class LocalStorageMock {
-    constructor() {
-      this.store = {};
-    }
-
-    clear() {
-      this.store = {};
-    }
-
-    getItem(key) {
-      return this.store[key] || null;
-    }
-
-    setItem(key, value) {
-      this.store[key] = String(value);
-    }
-
-    removeItem(key) {
-      delete this.store[key];
-    }
-  }
-
-  global.localStorage = new LocalStorageMock();
-
-  return {
-    save: jest.fn((key, value) => global.localStorage.setItem(key, value)),
-    load: jest.fn((key) => global.localStorage.getItem(key)),
-  };
-});
+jest.mock("../../../storage/index.js");
 
 const successResponse = (status = "200", statusText = "Ok") => {
   return Promise.resolve({

d817d397ea3d9040717909de88e9d67d56abe656
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-01-29 11:01:44 +0100

    adds failur test to login

--- src/js/api/auth/__tests__/login.test.js
+++ src/js/api/auth/__tests__/login.test.js
@@ -46,14 +46,28 @@ const successResponse = (status = "200", statusText = "Ok") => {
   });
 };
 
+const failureResponse = (status = "401", statusText = "Unauthorized") => {
+  return Promise.resolve({
+    ok: false,
+    status,
+    statusText,
+  });
+};
+
 const fetchSuccessMock = jest.fn(() => successResponse());
+const fetchFailureMock = jest.fn(() => failureResponse());
 
 describe("login", () => {
-  it("logs in the user and saves the token and profile", async () => {
+  it("logs in the user with valid credentials and saves the token", async () => {
     global.fetch = fetchSuccessMock;
     const profile = await login();
     const { token } = profile;
     save("token", token);
     expect(save).toHaveBeenCalledWith("token", token);
+  });
+
+  it("throws an error when user types in invalid credentials", async () => {
+    global.fetch = fetchFailureMock;
+    await expect(login()).rejects.toThrow("Unauthorized");
   });
 });
